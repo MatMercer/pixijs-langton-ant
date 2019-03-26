e9cfe04edcef1d7ed2d598180110da3d494c410e
MatMercer <accounts@matbm.net>
2019-03-26 13:06:17 -0300

    Major refactor

--- src/ant-world.js
+++ src/ant-world.js
@@ -2,23 +2,29 @@ const AntMath = require('./ant-math');
 
 class LangtonWorld {
     constructor(size) {
-        this.grid = [];
-
-        for (let i = 0; i < size; i += 1) {
-            this.grid[i] = new Uint32Array(size);
-        }
-
         this.size = size;
+        this._generateGrid();
     }
 
     increment(coord, maxInc) {
-        this.grid[coord[0]][coord[1]] = AntMath.cicle(1, Math.max(1, this.grid[coord[0]][coord[1]]) + 1, maxInc);
-
-        return this.grid[coord[0]][coord[1]] - 1;
+        return this._incrementPos(coord, maxInc) - 1;
     }
 
     value(coord) {
         return this.grid[coord[0]][coord[1]] - 1;
+    }
+
+    _generateGrid() {
+        this.grid = [];
+
+        for (let i = 0; i < this.size; i += 1) {
+            this.grid[i] = new Uint32Array(this.size);
+        }
+    }
+
+    _incrementPos(coord, maxInc) {
+        return this.grid[coord[0]][coord[1]] = 
+            AntMath.cicle(1, Math.max(1, this.grid[coord[0]][coord[1]]) + 1, maxInc);
     }
 }
 
--- src/ant.js
+++ src/ant.js
@@ -1,7 +1,20 @@
 const LangtonWorld = require('./ant-world');
 const AntMath = require("./ant-math");
 
+const UP = [0, 1];
+const RIGHT = [1, 0];
+const DOWN = [0, -1];
+const LEFT = [-1, 0];
+
+const DIRECTION = [
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT
+];
+
 class LangtonAnt {
+
     /**
      * Langton Ant with options
      * @param {Object} options - The options that control the ant, like wolrd size
@@ -9,22 +22,14 @@ class LangtonAnt {
      * @param {array[int]} options.program - A bit array describing the ant program
      */
     constructor(options) {
-        // Allocate world
-        this.world = new LangtonWorld(options.worldSize);
-
         this.program = options.program;
-
+        this.world = new LangtonWorld(options.worldSize);
         this.coord = new Uint32Array(2);
+
         this.ground = 0;
+        this.direction = 0;
 
-        this.rotArr = [
-            [0, 1], // UP
-            [1, 0], // RIGHT
-            [0, -1], // DOWN
-            [-1, 0] // LEFT
-        ];
-        this.rotIdx = 0;
-
+        // TODO: the user can setup where the ant starts
         this.coord[0] = Math.ceil(this.world.size / 2);
         this.coord[1] = Math.ceil(this.world.size / 2);
 
@@ -33,23 +38,34 @@ class LangtonAnt {
     }
 
     next() {
-        this.ground = this.world.increment(this.coord, this.program.length);
-
-        this.move(this.program[this.ground]);
-
+        this._move();
         this.steps += 1;
     }
 
-    move(command) {
-        this.rotIdx += command === 0 ? -1 : 1;
-        this.rotIdx = AntMath.cicle(0, this.rotIdx, 3);
-
-        this.incrementCoord(this.rotArr[this.rotIdx]);
+    _move() {
+        this._incrementGround();
+        this._rotate();
+        this._walk();
     }
 
-    incrementCoord(dir) {
+    _incrementGround() {
+        this.ground = this.world.increment(this.coord, this.program.length);
+    }
+
+    _rotate() {
+        const goToRight = this.program[this.ground] === 1;
+        this.direction += goToRight ? 1:-1;
+        this._nextDirection();
+    }
+
+    _walk() {
+        const dir = DIRECTION[this.direction];
         this.coord[0] += dir[0];
         this.coord[1] += dir[1];
+    }
+
+    _nextDirection() {
+        this.direction = AntMath.cicle(0, this.direction, DIRECTION.length - 1);
     }
 }
 
