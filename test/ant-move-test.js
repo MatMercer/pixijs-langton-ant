695ba842e45f94901209d4045a65d1060444d0c4
MatMercer <contact@matbm.net>
2019-03-27 13:08:36 -0300

    Add move test

--- test/ant-move-test.js
+++ test/ant-move-test.js
@@ -4,6 +4,28 @@ const AntTester = require('./ant-tester');
 const LangtonAnt = require('../src/ant');
 
 describe('Langton Ant', function () {
+    describe("#steps", function () {
+        const ant = new LangtonAnt({
+            worldSize: antPrograms[0].worldSize,
+            program: antPrograms[0].program
+        });
+
+        it("have the right ammount of steps after permuting fowards", function () {
+            AntTester.permuteAntFowards(ant, 1000);
+            assert.deepStrictEqual(ant.steps, 1000);
+            AntTester.permuteAntFowards(ant, 33);
+            assert.deepStrictEqual(ant.steps, 1033);
+            AntTester.permuteAntBackwards(ant, 33);
+        });
+
+        it("have the right ammount of steps after permuting backwards", function () {
+            AntTester.permuteAntBackwards(ant, 500);
+            assert.deepStrictEqual(ant.steps, 500);
+            AntTester.permuteAntBackwards(ant, 200);
+            assert.deepStrictEqual(ant.steps, 300);
+        });
+    });
+
     describe('#move()', function () {
         antPrograms.forEach(program => {
             program.results.forEach(result => {
